358f23684d6527f683ba88ff8f1f24b30ab193fd
Sito8943 <sito8943@gmail.com>
2022-07-18 02:03:34 -0700

    doing token given

--- routes/auth.js
+++ routes/auth.js
@@ -1,18 +1,31 @@
 const express = require("express");
 // chalk
 const { error, log } = require("../utils/chalk");
-const { login } = require("../utils/auth/functions");
+const { login, register } = require("../utils/auth/functions");
 
 const router = express.Router();
 
 const load = require("../utils/loading");
 
-//return all data
 router.post("/login", async (req, res) => {
   load.start();
   try {
     const { user, password } = req.body;
     const result = await login(user, password);
+    if (result.error == undefined) res.send(result);
+    else res.send({ error: result.error });
+  } catch (err) {
+    log(error(err));
+    res.sendStatus(500);
+  }
+  load.stop();
+});
+
+router.post("/register", async (req, res) => {
+  load.start();
+  try {
+    const { user, password } = req.body;
+    const result = await register(user, password);
     if (result.error == undefined) res.send(result);
     else res.send({ error: result.error });
   } catch (err) {
--- utils/auth/functions.js
+++ utils/auth/functions.js
@@ -1,10 +1,24 @@
+// @ts-check
+
+const uuid = require("node-uuid");
+
 const { GetValue, Insert } = require("../../db/local");
 
+/**
+ *
+ * @param {string} user
+ * @param {string} password
+ * @returns user data
+ */
 const login = async (user, password) => {
   let theUser = {};
   try {
     const data = GetValue("users", user.toLowerCase());
     if (data !== undefined) {
+      const date = new Date();
+      const stringDate = `${date.getFullYear()}-${
+        date.getMonth() + 1
+      }-${date.getDate()} ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}}`;
       theUser.n = data.n;
       theUser.p = data.p;
       theUser.m = data.m;
@@ -14,6 +28,8 @@ const login = async (user, password) => {
           data: {
             user,
             m: theUser.m,
+            token: uuid.v4(),
+            expiration: stringDate,
           },
         };
       else return { status: 422, data: { error: "wrong password" } };
@@ -27,7 +43,7 @@ const login = async (user, password) => {
 const register = async (user, password) => {
   try {
     const data = GetValue("users", user.toLowerCase());
-    if (data !== undefined) {
+    if (data === undefined) {
       Insert("users", user.toLowerCase(), { user, password });
       return {
         status: 200,
