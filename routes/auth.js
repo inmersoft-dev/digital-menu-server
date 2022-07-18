a09674035d4cea9523004e4eda60281aec68a14e
Sito8943 <sito8943@gmail.com>
2022-07-18 01:55:35 -0700

    doing register

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const express = require("express");
-
+// chalk
+const { error, log } = require("../utils/chalk");
 const { login } = require("../utils/auth/functions");
 
 const router = express.Router();
@@ -14,7 +15,8 @@ router.post("/login", async (req, res) => {
     const result = await login(user, password);
     if (result.error == undefined) res.send(result);
     else res.send({ error: result.error });
-  } catch (error) {
+  } catch (err) {
+    log(error(err));
     res.sendStatus(500);
   }
   load.stop();
--- utils/auth/functions.js
+++ utils/auth/functions.js
@@ -1,5 +1,48 @@
-const login = async (user, password) => {};
+const { GetValue, Insert } = require("../../db/local");
+
+const login = async (user, password) => {
+  let theUser = {};
+  try {
+    const data = GetValue("users", user.toLowerCase());
+    if (data !== undefined) {
+      theUser.n = data.n;
+      theUser.p = data.p;
+      theUser.m = data.m;
+      if (theUser.p.toLowerCase() === password.toLowerCase())
+        return {
+          status: 200,
+          data: {
+            user,
+            m: theUser.m,
+          },
+        };
+      else return { status: 422, data: { error: "wrong password" } };
+    }
+    return { status: 422, data: { error: "not found" } };
+  } catch (err) {
+    return { error: String(err) };
+  }
+};
+
+const register = async (user, password) => {
+  try {
+    const data = GetValue("users", user.toLowerCase());
+    if (data !== undefined) {
+      Insert("users", user.toLowerCase(), { user, password });
+      return {
+        status: 200,
+        data: {
+          user,
+        },
+      };
+    }
+    return { status: 422, data: { error: "username taken" } };
+  } catch (err) {
+    return { error: String(err) };
+  }
+};
 
 module.exports = {
   login,
+  register,
 };
