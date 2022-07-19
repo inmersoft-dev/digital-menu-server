be512414e3c79b2e6390cf27ebab7b5072b58334
Sito8943 <sito8943@gmail.com>
2022-07-19 14:49:09 -0700

    working on imagekit

---
+++ config.js
@@ -0,0 +1,9 @@
+const { IMAGEKIT_URL, IMAGEKIT_PUBLIC_KEY, IMAGEKIT_PRIVATE_KEY } = process.env;
+
+const config = {
+  imagekitUrl: IMAGEKIT_URL,
+  imagekitPublicKey: IMAGEKIT_PUBLIC_KEY,
+  imagekitAuthUrl: IMAGEKIT_PRIVATE_KEY,
+};
+
+export default config;
--- index.js
+++ index.js
@@ -11,6 +11,14 @@ const {
 
 const app = express();
 
+const ImageKit = require("imagekit");
+
+const imagekit = new ImageKit({
+  urlEndpoint: "<YOUR_IMAGEKIT_URL_ENDPOINT>",
+  publicKey: "<YOUR_IMAGEKIT_PUBLIC_KEY>",
+  privateKey: "<YOUR_IMAGEKIT_PRIVATE_KEY>",
+});
+
 // middle wares
 // morgan
 app.use(morgan.assignId);
